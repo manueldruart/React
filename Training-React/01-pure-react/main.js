1ac7bb94d666a0c37ab783e7d7e9c3471bc68d51
manueldruart <manuel.druart@gmail.com>
2022-01-26 09:44:14 +0100

    Training React Push

---
+++ Training-React/01-pure-react/main.js
@@ -0,0 +1,5 @@
+const App = () => {
+    return React.createElement('div', { className: 'title'},React.createElement('h1', {}, 'Mon beau titre'));
+}
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
+
